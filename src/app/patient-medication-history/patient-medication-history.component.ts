735839cf5a5c15eaa4cb8ea4971b286546396430
julioallday <naruto481@gmail.com>
2023-04-20 01:34:30 +0000

    feat(compent): adiciona listagem de medicamentos

--- src/app/patient-medication-history/patient-medication-history.component.ts
+++ src/app/patient-medication-history/patient-medication-history.component.ts
@@ -1,10 +1,34 @@
+import { LocalStorageService } from './../shared/services/local-storage.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-patient-medication-history',
   templateUrl: './patient-medication-history.component.html',
-  styleUrls: ['./patient-medication-history.component.css']
+  styleUrls: ['./patient-medication-history.component.css'],
 })
 export class PatientMedicationHistoryComponent {
+  listaPacientes: any = [];
+  paciente: any = {};
+  historico: any = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private storage: LocalStorageService
+  ) {
+    this.getPatientById();
+  }
+  getPatientById() {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      this.storage.getStorage('pacientes')
+        ? (this.listaPacientes = this.storage.getStorage('pacientes'))
+        : [];
+      this.paciente = this.listaPacientes.find((el: any) => el.id == id) || {};
+      this.historico = this.paciente.medicamentos;
+    });
+  }
+  editarMedicacao(medicacao: any) {}
+  deletarMedicacao(medicacao: any) {}
+  cadastrarMedicacao() {}
 }
